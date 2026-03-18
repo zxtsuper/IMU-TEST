9bd677852ea403f99136ec26879be0faf197572a
zhangxutao <zxt436063945@163.com>
2026-03-18 14:22:14 +0800

    Default to y-axis heading output

--- matlab/north_finding_demo.m
+++ matlab/north_finding_demo.m
@@ -1,13 +1,35 @@
-% Update the default console output and convergence plot focus on heading of the body y-axis.
+% North Finding Demo Script
+% Author: zxtsuper
+% Date: 2026-03-18
+% This script demonstrates the process of finding north based on IMU data.
+% The primary output is the y-axis heading from North (CW+).
 
+% Load the IMU data
+load('imu_data.mat');
+
+% Process the data
+res = process_imu_data(imu_data);
+
+% Calculate y-axis heading (CW+)
 heading_y_deg = mod(res.heading_deg + 90, 360);
-fprintf('Primary Heading (Y-axis): %f degrees\n', heading_y_deg);
 
-% Update figure 3 to plot heading_y history and its 1-sigma bound
-plot(mod(yaw_history_deg + 90, 360));
+% Plot the results
+figure;
+plot(res.time, heading_y_deg, 'LineWidth', 2);
 hold on;
-% Code for 1-sigma bounds not shown in prompt
-% ... 
 
-% Finish plotting code here
+% Calculate 1-sigma band
+y_sigma = std(res.yaw_history_deg);
+plot(res.time, mod(res.yaw_history_deg + 90, 360) + y_sigma, 'r--');
+plot(res.time, mod(res.yaw_history_deg + 90, 360) - y_sigma, 'r--');
+
+% Figure attributes
+title('Y-Axis Heading from North (CW+)');
+xlabel('Time (s)');
+ylabel('Heading (degrees)');
+grid on;
+legend('Y-Axis Heading', '1-Sigma Band');
 hold off;
+
+% Save figure
+saveas(gcf, 'y_axis_heading_plot.png');
